b5d1bc9cfcaed0aef8a2401aaeff8440061839a7
Diego Molina <diemol@gmail.com>
2018-04-20 08:15:56 +0200

    Example

---
+++ backend/events.js
@@ -0,0 +1,19 @@
+module.exports = [
+    {
+      id: 1,
+      title: 'Selenium and Docker Workshop - Day 1',
+      detail: 'Using Docker to simplify and improve testing with Selenium',
+      date: '2018-04-19'
+    },
+    {
+      id: 2,
+      title: 'Selenium and Docker Workshop - Day 2',
+      detail: 'Using Docker to simplify and improve testing with Selenium',
+      date: '2018-04-20'
+    },
+    {
+      id: 3,
+      title: 'Selenium Conference India 2018',
+      date: '2018-06-28'
+    }
+  ];
---
+++ bulletin_board.rb
@@ -0,0 +1,24 @@
+require 'watir'
+browser = Watir::Browser.new :chrome, url: "http://hub:4444/wd/hub"
+
+caps = Selenium::WebDriver::Remote::Capabilities.new
+
+caps["name"] = "Bulletin Board Test Example"
+caps["browserName"] = "chrome"
+caps["screenResolution"] = "1024x768"
+caps["recordVideo"] = "true"
+
+browser = Watir::Browser.new(
+    :remote,
+    :url => "http://hub:4444/wd/hub",
+    :desired_capabilities => caps)
+  
+browser.goto 'http://bulletin:8080'
+title = browser.text_field css: 'input[class="form-control"][placeholder="Title"]'
+title.set 'Party'
+btn = browser.button value: 'Submit'
+btn.click
+
+puts browser.title
+# => Bulletin Board
+browser.quit
